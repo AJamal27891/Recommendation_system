dc1bf2475055fc952c44279eff28ee8ee5cb3f3b
Ahmed Gamal <33566147+AJamal27891@users.noreply.github.com>
2021-07-26 22:22:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ products = pd.read_json(r'meta.json').T
 #antecedents 	consequents 	productid 	brand 	category 	subcategory 	name 	
 #productid_right 	brand_right 	category_right 	subcategory_right 	name_right
 
-def get_productdata(id):
+def get_productdata(id,br,cat,subcat,n):
   if id in meta.productid.values:
     df = meta.rename(columns={'productid':'productid_right', 	'brand':'brand_right',
                               'category':'category_right', 	'subcategory':'subcategory_right', 'name':	'name_right'})
@@ -28,14 +28,17 @@ def get_productdata(id):
     print(len(meta.productid))
     cols = ['productid', 	'brand', 	'category', 	'subcategory', 	'name']
     for c in cols :
-      df[c] = meta[meta['productid'] == id ][c].values[0]
-
-    df = df[['antecedents', 	'consequents', 	'productid', 	'brand', 	'category', 	'subcategory', 	'name',
-            'productid_right', 	'brand_right', 'category_right',	'subcategory_right', 	'name_right']]
-    return df
+        df[c] = meta[meta['productid'] == id ][c].values[0]
+     
   else :
-    return None
-
+    df['productid'] = id
+    df['brand'] = br
+    df['category'] = cat
+    df['subcategory'] = subcat
+    df['name'] = n
+  df = df[['antecedents', 	'consequents', 	'productid', 	'brand', 	'category', 	'subcategory', 	'name',
+            'productid_right', 	'brand_right', 'category_right',	'subcategory_right', 	'name_right']]
+  return df
 def pipline(df):
   corpus_list = []
   for seq in df.values:
@@ -62,8 +65,12 @@ app = Flask(__name__)
 
 @app.route('/recommend', methods=['POST'])
 def infer_recommender():
-    id = request.get_json('id')
-    df = get_productdata(id)
+    id = request.get_json('productid')
+    br =request.get_json('brand')
+    cat = request.get_json('category')
+    subcat = request.get_json('subcategory')
+    n = request.get_json('name')
+    df = get_productdata(id,br,cat,subcat,n)
     if df:
       return jsonify(pipline(df))
     else :
